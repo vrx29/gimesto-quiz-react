3dd4454adb2d3cfeaad8f8aadf874923ce86acfd
Vineet Kumar <jammackp@gmail.com>
2022-04-16 22:38:06 +0530

    refactor: create array for dynamic listing of rules

---
+++ src/data/rules.ts
@@ -0,0 +1,7 @@
+export const rules = [
+  "There will be 5 questions.",
+  "Each question will be of one mark.",
+  "There will be four options provided.",
+  "You have to select only one option.",
+  "Score must be more than 60% to win.",
+];
--- src/pages/Rules/index.tsx
+++ src/pages/Rules/index.tsx
@@ -1,46 +1,25 @@
 import "./rules.css";
-import rules from "assets/hero/rules.png";
+import rulesIcon from "assets/hero/rules.png";
 import React from "react";
 import { FingerRight } from "assets/icons";
 import { Link } from "react-router-dom";
+import { rules } from "data/rules";
 
 export function Rules() {
   return (
     <section>
       <div className="card">
-        <img src={rules} alt="rules__page--img" />
+        <img src={rulesIcon} alt="rules__page--img" />
         <h6>Rules to be followed</h6>
         <ul>
-          <li>
-            <span>
-              <FingerRight className="icon-arrow" />
-            </span>
-            There will be 5 questions.
-          </li>
-          <li>
-            <span>
-              <FingerRight className="icon-arrow" />
-            </span>
-            Each question will be of one mark.
-          </li>
-          <li>
-            <span>
-              <FingerRight className="icon-arrow" />
-            </span>
-            There will be four options provided.
-          </li>
-          <li>
-            <span>
-              <FingerRight className="icon-arrow" />
-            </span>
-            You have to select only one option.
-          </li>
-          <li>
-            <span>
-              <FingerRight className="icon-arrow" />
-            </span>
-            Score must be more than 60% to win.
-          </li>
+          {rules.map((rule) => (
+            <li>
+              <span>
+                <FingerRight className="icon-arrow" />
+              </span>
+              {rule}
+            </li>
+          ))}
         </ul>
         <Link className="btn btn-primary" to="/quiz">
           Start Quiz
